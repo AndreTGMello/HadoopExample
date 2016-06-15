520faf2d1ea2bfb21de609a528791d15b25376f9
André Thomaz Gandolpho de Mello <andre.tgmello@gmail.com>
2016-06-15 18:06:08 -0300

    Maven project.

--- src/mapreduce/AggregateJob.java
+++ src/mapreduce/AggregateJob.java
@@ -1,5 +1,17 @@
 package mapreduce;
 
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.reduce.LongSumReducer;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
 public class AggregateJob {
 
+	
 }
