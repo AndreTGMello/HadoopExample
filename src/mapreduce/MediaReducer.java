aa4e75077ef4f2d8937359f0d32a804a4c416254
André Thomaz Gandolpho de Mello <andre.tgmello@gmail.com>
2016-06-20 22:01:11 -0300

    Utiliza DoubleWritable corretamente em reducer.

--- src/mapreduce/MediaReducer.java
+++ src/mapreduce/MediaReducer.java
@@ -6,11 +6,11 @@ import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Reducer;
  
-public class MediaReducer<KEY> extends Reducer<KEY, CompositeWritable, KEY, CompositeWritable> {
+public class MediaReducer<KEY> extends Reducer<KEY, DoubleWritable, KEY, CompositeWritable> {
  
 	private CompositeWritable resultSet = null;
   
-	public void reduce(KEY key, Iterable<CompositeWritable> values, Context context) throws IOException, InterruptedException {
+	public void reduce(KEY key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
 		int n = 0;
 		double soma = 0;
 		double variancia = 0;
@@ -22,14 +22,14 @@ public class MediaReducer<KEY> extends Reducer<KEY, CompositeWritable, KEY, Comp
 			n++;
 		}
 */
-		for (CompositeWritable compWrite : values) {
-			soma += compWrite.getValor();
+		for (DoubleWritable dw : values) {
+			soma += dw.get();
 			n++;
 		}
 		media = soma/n;
 		
-		for (CompositeWritable compWrite : values) {
-			variancia += Math.pow((compWrite.getValor()-media), 2);
+		for (DoubleWritable dw : values) {
+			variancia += Math.pow((dw.get()-media), 2);
 		}
 		variancia = variancia/(n-1);
 		desvioPadrao = Math.sqrt(variancia);
