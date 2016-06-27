821cd814c9603c6c41aa32dc72cd27537abd00bb
André Thomaz Gandolpho de Mello <andre.tgmello@gmail.com>
2016-06-27 10:23:09 -0300

    Implementa serializable, para poder fazer regressao.

--- src/mapreduce/CompositeWritable.java
+++ src/mapreduce/CompositeWritable.java
@@ -3,18 +3,32 @@ package mapreduce;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.io.Serializable;
 import java.text.DecimalFormat;
 import java.math.RoundingMode;
 
 import org.apache.hadoop.io.Writable;
 
-public class CompositeWritable implements Writable {
-    double valor = 0.0;
-	double media = 0.0;
-    double desvioPadrao = 0.0;
-    double variancia = 0.0;
-    
-    public double getValor() {
+public class CompositeWritable implements Writable, Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3820756035070415554L;
+	private double valor = 0.0;
+	private double media = 0.0;
+	private double desvioPadrao = 0.0;
+	private double variancia = 0.0;
+	private String agrupadorX = "";
+	private double estatisticaY = 0.0;
+	private double xMin = 0.0;
+	private double xMax = 0.0;
+	private double a = 0.0;
+	private double b = 0.0;
+	private boolean flag = false;
+	// Flag false = escreve estatisticas
+	// Flag true = escreve reta
+
+	public double getValor() {
 		return valor;
 	}
 
@@ -47,43 +61,53 @@ public class CompositeWritable implements Writable {
 	}
 
 	public CompositeWritable() {}
-    
-    public CompositeWritable(double valor) {
-    	this.valor = valor;
-    }
 
-    public CompositeWritable(double media, double desvioPadrao, double variancia) {
-    	this.media = media;
-        this.desvioPadrao = desvioPadrao;
-        this.variancia = variancia;
-    } 
+	public CompositeWritable(double valor) {
+		this.valor = valor;
+	}
 
+	public CompositeWritable(double media, double desvioPadrao, double variancia) {
+		this.media = media;
+		this.desvioPadrao = desvioPadrao;
+		this.variancia = variancia;
+	} 
 
-    public void readFields(DataInput in) throws IOException {
-        valor = in.readInt();
-    	media = in.readDouble();
-        desvioPadrao = in.readDouble();
-        variancia = in.readDouble();
-    }
+	public CompositeWritable(String x, double y) {
+		this.estatisticaY = y;
+		this.agrupadorX = x;
+		this.flag = true;
+	} 
 
-    public void write(DataOutput out) throws IOException {
-    	out.writeDouble(valor);
-    	out.writeDouble(media);
-        out.writeDouble(desvioPadrao);
-        out.writeDouble(variancia);
-    }
+	public void readFields(DataInput in) throws IOException {
+		valor = in.readInt();
+		media = in.readDouble();
+		desvioPadrao = in.readDouble();
+		variancia = in.readDouble();
+	}
 
-    public void merge(CompositeWritable other) {
-    	this.valor += other.valor;
+	public void write(DataOutput out) throws IOException {
+		out.writeDouble(valor);
+		out.writeDouble(media);
+		out.writeDouble(desvioPadrao);
+		out.writeDouble(variancia);
+	}
+
+	public void merge(CompositeWritable other) {
+		this.valor += other.valor;
 		this.media += other.media;
 		this.desvioPadrao += other.desvioPadrao;
 		this.variancia += other.variancia;
-    }
+	}
 
-    @Override
-    public String toString() {
-    	DecimalFormat df = new DecimalFormat("#.#");
-        df.setRoundingMode(RoundingMode.CEILING);
-        return df.format(this.media) + "\t" + df.format(this.desvioPadrao) + "\t" + df.format(this.variancia);
-    }
+	@Override
+	public String toString() {
+		if(flag){
+			return this.a + "\t" + this.b + "\t" + this.xMin + "\t" + this.xMax; 
+		}else{
+			DecimalFormat df = new DecimalFormat("#.#");
+			df.setRoundingMode(RoundingMode.CEILING);
+			return df.format(this.media) + "\t" + df.format(this.desvioPadrao) + "\t" + df.format(this.variancia);
+		}
+
+	}
 }
