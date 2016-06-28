5c4a3364ad3a2b6c56f4567b3c0f877f343ad860
CinthiaMV <cinthia.vlj@gmail.com>
2016-06-28 18:29:25 -0300

    Grafico legivel

--- src/mapreduce/Grafico.java
+++ src/mapreduce/Grafico.java
@@ -1,16 +1,23 @@
 package mapreduce;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPosition;
+import org.jfree.chart.axis.CategoryLabelPositions;
 import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.xy.XYDataset;
@@ -23,9 +30,44 @@ public class Grafico extends ApplicationFrame {
 	public static String caminhoPrimeiraSaida = "";
 	public static String caminhoSegundaSaida = "";
 	public static String estatistica = "";
+	public static ArrayList<String> categorias = new ArrayList<String>();
     public Grafico(final String title) throws FileNotFoundException {
         super(title);
         final JFreeChart chart = ChartFactory.createLineChart("Grafico", "Periodo", "Estatistica", createDataset(), PlotOrientation.VERTICAL, true, true, false);
+        CategoryPlot plot = (CategoryPlot) chart.getPlot();
+        LineAndShapeRenderer renderer = (LineAndShapeRenderer) plot.getRenderer();
+        renderer.setSeriesShapesVisible(0, true);
+        renderer.setSeriesShapesVisible(1, true);
+        CategoryAxis axis = chart.getCategoryPlot().getDomainAxis();
+        int baldeCategoria = 1;
+        double proporcaoD = (categorias.size()+10)/41; 
+        int proporcao = (int) proporcaoD;
+        int indiceCategoria = 0;
+        for(; indiceCategoria < categorias.size(); indiceCategoria++)
+        {
+        	axis.setTickLabelPaint(categorias.get(indiceCategoria), Color.white);
+        	if(baldeCategoria%proporcao == 0)
+        	{
+        		axis.setTickLabelPaint(categorias.get(indiceCategoria), Color.black);
+        	}
+        	else
+        		axis.setTickLabelFont(categorias.get(indiceCategoria),new Font("Helvetica", Font.PLAIN, 0));
+        	baldeCategoria++;
+        }
+        for(indiceCategoria = 0; indiceCategoria < 10; indiceCategoria++)
+        {
+        	axis.setTickLabelPaint("P"+indiceCategoria, Color.white);
+        	if(baldeCategoria%proporcao == 0)
+        	{
+        		axis.setTickLabelPaint("P"+indiceCategoria, Color.black);
+        	}
+        	else
+        		axis.setTickLabelFont("P"+indiceCategoria,new Font("Helvetica", Font.PLAIN, 0));
+        	baldeCategoria++;
+        }
+        axis.setCategoryLabelPositions(CategoryLabelPositions.UP_90);
+        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setVerticalTickLabels(true);
         final ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
         setContentPane(chartPanel);
@@ -38,13 +80,10 @@ public class Grafico extends ApplicationFrame {
     	Scanner scan = new Scanner(f);
     	double a = 0.0;
     	double b = 0.0;
-    	int anoIniBalde = 0; 
-    	//String anoIni = "";
     	double resultadofuncao = 0.0;
     	scan.next();
     	a = Double.parseDouble(scan.next());
     	b = Double.parseDouble(scan.next());
-    	anoIniBalde = (int) (Double.parseDouble(scan.next()));//para tirar a casa decimal do ano
     	scan.close();
         f = new File(caminhoPrimeiraSaida+"/"+moldeArquivo);
         scan = new Scanner(f);
@@ -62,6 +101,7 @@ public class Grafico extends ApplicationFrame {
     	while(scan.hasNext()){//faz primeira reta
     		
     		x = scan.next();
+    		categorias.add(x);
     		int z = 0;
     		for(z = 0; z < indiceEstatistica; z++)
     		{
@@ -82,74 +122,11 @@ public class Grafico extends ApplicationFrame {
         	dataset.addValue(resultadofuncao, "previsao", "P"+Integer.toString(h));
         	indiceRegressao++;
     	}
-    	
-    	//faz segunda reta?
-    	//faz terceira reta
     	scan.close();
-    	
-    	//anoIni = Integer.toString(anoIniBalde);
-    	/*for(int ano = anoIniBalde; ano < anoIniBalde+20; ano++){
-    		resultadofuncao = a + b*ano;
-    		dataset.addValue(resultadofuncao, "previsao", Integer.toString(ano));
-    	}*/
         return dataset;
         
     }
-   
-   /* private JFreeChart createChart(final XYDataset dataset) {
-        
-        final JFreeChart chart = ChartFactory.createXYLineChart(
-            "Grafico",      // chart title
-            "Periodo de Tempo",                      // x axis label
-            "Valor da"+estatistica,                      // y axis label
-            dataset,                  // data
-            PlotOrientation.VERTICAL,
-            true,                     // include legend
-            true,                     // tooltips
-            false                     // urls
-        );
-
-        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
-        chart.setBackgroundPaint(Color.white);
-
-//        final StandardLegend legend = (StandardLegend) chart.getLegend();
-  //      legend.setDisplaySeriesShapes(true);
-        
-        // get a reference to the plot for further customisation...
-        final XYPlot plot = chart.getXYPlot();
-        plot.setBackgroundPaint(Color.lightGray);
-    //    plot.setAxisOffset(new Spacer(Spacer.ABSOLUTE, 5.0, 5.0, 5.0, 5.0));
-        plot.setDomainGridlinePaint(Color.white);
-        plot.setRangeGridlinePaint(Color.white);
-        
-        final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-        plot.setRenderer(renderer);
-
-        // change the auto tick unit selection to integer units only...
-        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
-        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
-        // OPTIONAL CUSTOMISATION COMPLETED.
-                
-        return chart;
-        
-    }*/
-
-    // ****************************************************************************
-    // * JFREECHART DEVELOPER GUIDE                                               *
-    // * The JFreeChart Developer Guide, written by David Gilbert, is available   *
-    // * to purchase from Object Refinery Limited:                                *
-    // *                                                                          *
-    // * http://www.object-refinery.com/jfreechart/guide.html                     *
-    // *                                                                          *
-    // * Sales are used to provide funding for the JFreeChart project - please    * 
-    // * support us so that we can continue developing free software.             *
-    // ****************************************************************************
     
-    /**
-     * Starting point for the demonstration application.
-     *
-     * @param args  ignored.
-     */
     public static void criaGrafico(String argUm, String argDois, String argOito) {
 
         caminhoPrimeiraSaida = argUm + "/" + argDois;
